1592315b2218eaea919f59d9fa168f8084d3a34f
faruktoptas <faruktoptas@gmail.com>
2020-02-13 14:41:38 +0300

    Add support for custom font size, resize ratio and x,y positions

--- framer.py
+++ framer.py
@@ -5,65 +5,72 @@ import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-labelsFile = "framer.json"
-stringsFile = "strings.json"
-folder = "images"
+class Framer:
+	configFile = "framer.json"
+	stringsFile = "strings.json"
+	folder = "images"
+	
+	def __init__(self):
+		self.config = json.loads(open(self.configFile).read())
+		self.titles = json.loads(open(self.stringsFile).read())
+		self.bg = self.config["background"]
+		self.font = self.config["font"]
+		self.data = self.config["data"]
+		self.resizeRatio = self.config["resize"]
+		self.fontSize = self.config["fontsize"]
+		self.xPos = str(self.config["xposition"])
+		self.yPos = str(self.config["yposition"])
 
-labels = json.loads(open(labelsFile).read())
-titles = json.loads(open(stringsFile).read())
-bg = labels["background"]
-font = labels["font"]
-data = labels["data"]
+	def start(self):
+		folder = self.folder
+		files = os.listdir(folder)
+		langFolders = []
+		for f in files:
+			if(os.path.isdir(folder + "/" + f)):
+				langFolders.append(f)
+
+		for lang in langFolders:
+			for f in os.listdir(folder + "/" + lang ):
+				imgFolder = folder + "/" + lang + "/"
+				img = imgFolder + f
+				file = f[:f.rfind(".")]
+				ext = f[f.rfind("."):]
+				if (len(file) > 0 and unicode(file).isnumeric()):
+					resized = self.resize(imgFolder, file, ext)
+					framed = self.frame(imgFolder, resized, ext)
+					if (self.data.has_key(file)):
+						titleKeys = self.data[file]
+						title1 = ""
+						title2 = ""
+						if (self.titles.has_key(lang)):
+							if (len(titleKeys) > 0 and self.titles[lang].has_key(titleKeys[0])):
+								title1 = self.titles[lang][titleKeys[0]]
+							if (len(titleKeys) > 1 and self.titles[lang].has_key(titleKeys[1])):
+								title2 = self.titles[lang][titleKeys[1]]
+							self.label(framed, title1, title2)
+	
 
 
-def cmd(command):
-	return os.popen(command).read()
+	def cmd(self, command):
+		return os.popen(command).read()
 
-def __main__():
-	files = os.listdir(folder)
-	langFolders = []
-	for f in files:
-		if(os.path.isdir(folder + "/" + f)):
-			langFolders.append(f)
+	def resize(self, imgFolder, file, ext):
+		img = imgFolder + file  + ext
+		out = imgFolder + file + '_' + ext
+		self.cmd('convert ' + img + ' -resize %' + str(self.resizeRatio) + ' ' + out)
+		return file + "_"
 
-	for lang in langFolders:
-		for f in os.listdir(folder + "/" + lang ):
-			imgFolder = folder + "/" + lang + "/"
-			img = imgFolder + f
-			file = f[:f.rfind(".")]
-			ext = f[f.rfind("."):]
-			if (len(file) > 0 and unicode(file).isnumeric()):
-				resized = resize(imgFolder, file, ext)
-				framed = frame(imgFolder, resized, ext)
-				if (data.has_key(file)):
-					titleKeys = data[file]
-					title1 = ""
-					title2 = ""
-					if (titles.has_key(lang)):
-						if (len(titleKeys) > 0 and titles[lang].has_key(titleKeys[0])):
-							title1 = titles[lang][titleKeys[0]]
-						if (len(titleKeys) > 1 and titles[lang].has_key(titleKeys[1])):
-							title2 = titles[lang][titleKeys[1]]
-						label(framed, title1, title2)
+	def frame(self, imgFolder, file, ext):
+		img = imgFolder + file + ext
+		out = imgFolder + file + 'framed' + ext
+		self.cmd('convert ' + self.bg +' ' + img +' -geometry +' + self.xPos + '+' + self.yPos +' -composite ' + out)
+		return out
 
-def resize(imgFolder, file, ext):
-	ratio = 74.4
-	img = imgFolder + file  + ext
-	out = imgFolder + file + '_' + ext
-	cmd('convert ' + img + ' -resize %' + str(ratio) + ' ' + out)
-	return file + "_"
+	def label(self, img, title1, title2):
+		out = img.replace('_framed', '_out')
+		command = "convert " + img +" -font " + self.font + " -gravity North -fill white -pointsize " + self.fontSize + " -draw \"text 0,100 '" + title1 +"'\"  -draw \"text 0,220 '" + title2 + "'\" " + out
+		self.cmd(command)
+		self.cmd("rm " + img)
+		self.cmd("rm " + img.replace("_framed", "_"))
 
-def frame(imgFolder, file, ext):
-	img = imgFolder + file + ext
-	out = imgFolder + file + 'framed' + ext
-	cmd('convert ' + bg +' ' + img +' -geometry +243+583 -composite ' + out)
-	return out
-
-def label(img, title1, title2):
-	out = img.replace('_framed', '_out')
-	command = "convert " + img +" -font " + font + " -gravity North -fill white -pointsize 108 -draw \"text 0,100 '" + title1 +"'\"  -draw \"text 0,220 '" + title2 + "'\" " + out
-	cmd(command)
-	cmd("rm " + img)
-	cmd("rm " + img.replace("_framed", "_"))
-
-__main__()
+Framer().start()
